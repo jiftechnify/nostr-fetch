72074bc8c6061c66051eae876d8921a0e4069761
jiftechnify <jiftech.stlfy@gmail.com>
2023-03-01 19:55:30 +0900

    chore: add example

---
+++ examples/single.ts
@@ -0,0 +1,38 @@
+import "websocket-polyfill";
+import { eventKind, fetchAllEvents } from "../src/index";
+
+const nDaysAgo = (days: number): number =>
+  Math.floor(
+    new Date(Date.now() - days * 24 * 60 * 60 * 1000).getTime() / 1000
+  );
+
+if (process.argv.length <= 2) {
+  console.error("specify relay URL");
+  process.exit(1);
+}
+const relayUrl = process.argv[2] as string;
+
+const main = async () => {
+  // fetch all text events (kind: 1) posted in last week from the relay
+  const evs = await fetchAllEvents(
+    relayUrl,
+    {
+      since: nDaysAgo(7),
+    },
+    [
+      {
+        kinds: [eventKind.text],
+        // authors: ["<your pubkey (hex)>"],
+      },
+    ],
+    { verifyEventSig: false }
+  );
+  console.log(`fetched ${evs.length} events`);
+};
+
+main()
+  .then(() => console.log("fin"))
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
