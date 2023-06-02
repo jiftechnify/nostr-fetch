afffc842e47a02bd4f69c16421ef50300181db11
jiftechnify <jiftech.stlfy@gmail.com>
2023-06-03 00:31:53 +0900

    chore: add example: allEventsIter

---
+++ packages/examples/src/allEventsIter.ts
@@ -0,0 +1,38 @@
+import { NostrFetcher, eventKind } from "nostr-fetch";
+import "websocket-polyfill";
+
+import { defaultRelays, nHoursAgo } from "./utils";
+
+const main = async () => {
+  // initialize fetcher based on nostr-relaypool's `RelayPool`
+  const fetcher = NostrFetcher.init();
+
+  // fetch all text events (kind: 1) posted in the last hour from the relays
+  const eventsIter = await fetcher.allEventsIterator(
+    defaultRelays,
+    [
+      {
+        kinds: [eventKind.text],
+      },
+    ],
+    {
+      since: nHoursAgo(1),
+    },
+    {
+      skipVerification: true,
+    }
+  );
+
+  for await (const ev of eventsIter) {
+    console.log(ev.content);
+  }
+
+  fetcher.shutdown();
+};
+
+main()
+  .then(() => console.log("fin"))
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
