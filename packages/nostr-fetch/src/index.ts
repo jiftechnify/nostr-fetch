6da4515f3400f0ad080be9c6456bcdf3a2bf72a3
jiftechnify <jiftech.stlfy@gmail.com>
2023-06-26 00:09:26 +0900

    feat: export type `NostrFecthError` from nostr-fetch

--- packages/nostr-fetch/src/index.ts
+++ packages/nostr-fetch/src/index.ts
@@ -1,3 +1,4 @@
 export { NostrEvent, eventKind } from "@nostr-fetch/kernel/nostr";
 export { normalizeRelayUrl, normalizeRelayUrls } from "@nostr-fetch/kernel/utils";
 export * from "./fetcher";
+export { NostrFetchError } from "./fetcherHelper";
