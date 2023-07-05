d0045600837a432d31a5143654086d401ca56591
jiftechnify <jiftech.stlfy@gmail.com>
2023-07-05 21:22:08 +0900

    fix: respect NIP-01's note on reconnections to relays

--- packages/nostr-fetch/src/relayPool.ts
+++ packages/nostr-fetch/src/relayPool.ts
@@ -9,6 +9,10 @@ import {
   normalizeRelayUrls,
 } from "@nostr-fetch/kernel/utils";
 
+// [NIP-01](https://github.com/nostr-protocol/nips/blob/master/01.md#other-notes) says:
+// > When a websocket is closed by the relay with a status code 4000 that means the client shouldn't try to connect again.
+const CLOSE_CODE_RELAY_NOT_RECONNECTABLE = 4000;
+
 export interface RelayPool {
   ensureRelays(relayUrls: string[], relayOpts: RelayOptions): Promise<string[]>;
   getRelayIfConnected(relayUrl: string): Relay | undefined;
@@ -41,6 +45,7 @@ type ConnectFailedRelay = {
 type DisconnectedRelay = {
   state: "disconnected";
   relayUrl: string;
+  reconnectable: boolean;
 };
 type ManagedRelay = AliveRelay | ConnectingRelay | ConnectFailedRelay | DisconnectedRelay;
 
@@ -74,7 +79,7 @@ class RelayPoolImpl implements RelayPool {
     return (
       // TODO: make the threshold configuarable
       (relay.state === "connectFailed" && currUnixtimeMilli() - relay.failedAt > 60 * 1000) ||
-      relay.state === "disconnected" ||
+      (relay.state === "disconnected" && relay.reconnectable) ||
       (relay.state === "alive" && relay.relay.wsReadyState === WebSocket.CLOSED) // is it possible?
     );
   }
@@ -105,11 +110,19 @@ class RelayPoolImpl implements RelayPool {
           r.on("connect", () => logger?.log("info", `connected`));
           r.on("disconnect", (ev) => {
             logger?.log("info", `disconnected: ${JSON.stringify(ev)}`);
-            this.#relays.set(r.url, { state: "disconnected", relayUrl: r.url });
+            this.#relays.set(r.url, {
+              state: "disconnected",
+              relayUrl: r.url,
+              reconnectable: ev.code !== CLOSE_CODE_RELAY_NOT_RECONNECTABLE,
+            });
           });
           r.on("error", () => {
             logger?.log("error", `WebSocket error`);
-            this.#relays.set(r.url, { state: "disconnected", relayUrl: r.url });
+            this.#relays.set(r.url, {
+              state: "disconnected",
+              relayUrl: r.url,
+              reconnectable: true,
+            });
           });
           r.on("notice", (notice) => logger?.log("warn", `NOTICE: ${notice}`));
 
