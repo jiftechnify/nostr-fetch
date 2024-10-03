56cc8ea0cb547b2cae8a5b419b17116ec30b4fd5
jiftechnify <jiftech.stlfy@gmail.com>
2024-10-04 00:04:47 +0900

    add noopVerifier to exports

--- packages/kernel/src/utils.ts
+++ packages/kernel/src/utils.ts
@@ -1,3 +1,5 @@
+import type { EventVerifier, NostrEvent } from "./nostr";
+
 /**
  * Current unxitime in milliseconds.
  */
@@ -98,3 +100,15 @@ export const withTimeout = async <T>(
 
   return Promise.race([promise.finally(() => clearTimeout(timer)), timeout]);
 };
+
+/**
+ * no-op `EventVerifier` which regards all events have a valid signature.
+ *
+ * @example
+ * // emulating `skipVerification: true`
+ * import { NostrFetcher, noopVerifier } from "nostr-fetch";
+ *
+ * const fetcher = NostrFetcher.init();
+ * const events = fetcher.allEventsIterator({ ... }, { ... }, { eventVerifier: noopVerifier });
+ */
+export const noopVerifier: EventVerifier = (_: NostrEvent) => true;
